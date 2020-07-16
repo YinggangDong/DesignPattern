bec1e18acecf11f70e2a45414749dee36b52e44a
dongyinggang <dongyinggang@163.com>
2020-07-16 08:43:56 +0800

    引入spring-jdbc包,查看命令模式源码

---
+++ src/main/java/cn/dyg/designpattern/command/spring/SpringSourceCodeTest.java
@@ -0,0 +1,15 @@
+package cn.dyg.designpattern.command.spring;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * SpringSourceCodeTest 类是 命令模式在Spring源码中的体现
+ *
+ * @author dongyinggang
+ * @date 2020-07-15 19:47
+ **/
+public class SpringSourceCodeTest {
+    public static void main(String[] args) {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+    }
+}
